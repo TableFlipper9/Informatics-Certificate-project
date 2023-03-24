f88030a1826a5f76cf6359f0c888e1d3bccf9090
TableFlipper9 <123597091+TableFlipper9@users.noreply.github.com>
2023-03-24 21:10:25 +0200

    Delete Atestat directory

--- Atestat/Building.gd
+++
@@ -1,9 +0,0 @@
-extends MarginContainer
-
-func init(titlu , cost , image):
-	$Button/HBoxContainer/VBoxContainer/Label.text = titlu
-	#$HBoxContainer/TextureRect.texture = image
-	$Button/HBoxContainer/VBoxContainer/HBoxContainer/Label2.text = cost
-	
-func _ready():
-	pass
